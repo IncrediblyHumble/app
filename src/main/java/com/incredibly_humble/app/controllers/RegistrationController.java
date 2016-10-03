9bf25e158995ed44976051bf17fb5660f96ed854
Derek Henry <derek.james.henry@gmail.com>
2016-10-03 18:42:47 -0400

    ComboBox Addition

--- src/main/java/com/incredibly_humble/app/controllers/RegistrationController.java
+++ src/main/java/com/incredibly_humble/app/controllers/RegistrationController.java
@@ -1,9 +1,12 @@
 package com.incredibly_humble.app.controllers;
 
 import com.google.inject.Inject;
+import com.incredibly_humble.app.models.User;
 import com.incredibly_humble.app.util.Login;
 import com.incredibly_humble.app.util.impl.ScreenSwitch;
 import com.incredibly_humble.app.util.impl.TriesExceededException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -23,6 +27,9 @@ public class RegistrationController {
 
     @FXML
     private TextField passField;
+
+    @FXML
+    private ComboBox accountBox;
 
     @Inject
     Login newUser;
@@ -36,6 +43,18 @@ public class RegistrationController {
     private void handleCancelPressed(ActionEvent event) throws IOException {
         Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         screenSwitch.toScreen(primaryStage, "/views/login.fxml");
+    }
+
+    @FXML
+    private void initialize() {
+        ObservableList<User.AccountType> accountTypes = wrapper(User.AccountType.values());
+        accountBox.getItems().addAll(accountTypes);
+    }
+
+    private static ObservableList<User.AccountType> wrapper(User.AccountType[] accountTypes) {
+        ObservableList<User.AccountType> accountTypeList = FXCollections.observableArrayList();
+        accountTypeList.addAll(accountTypes);
+        return accountTypeList;
     }
     //TO IMPLEMENT: THE COMBO BOX WITH OPTIONS FOR USER TYPE
     // ObservableList<String> options =
