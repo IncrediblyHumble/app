1acb99e91f8db17ca04b987f1a4f47026497eef3
Maddie Ravichandran <ramaddie07@gatech.edu>
2016-10-03 20:07:59 -0400

    fixed control profile

--- src/main/java/com/incredibly_humble/app/controllers/ProfileController.java
+++ src/main/java/com/incredibly_humble/app/controllers/ProfileController.java
@@ -1,12 +1,19 @@
 package com.incredibly_humble.app.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.google.inject.Inject;
+import com.incredibly_humble.app.util.Login;
+import com.incredibly_humble.app.util.impl.ScreenSwitch;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ProfileController {
 
@@ -24,6 +31,10 @@ public class ProfileController {
     private Button save;
     @FXML
     private Label success;
+    @Inject
+    private Login user;
+    @Inject
+    private ScreenSwitch screenSwitch;
 
     public void saveProfile(ActionEvent event) {
         // TO IMPLEMENT: Saves current info put into profile Screen for the current user
@@ -32,8 +43,8 @@ public class ProfileController {
 
     }
 
-    public void cancelProfile(ActionEvent event) {
-        // TO IMPLEMENT
-        // Cancel out of profile page, erases what was in field before, go back to home screen
+    public void cancelProfile(ActionEvent event) throws IOException {
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        screenSwitch.toScreen(primaryStage, "/views/home.fxml");
     }
 }
