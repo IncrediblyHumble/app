9b5adc80375ccb7eab19021a52ae6a2f195f74a3
abahrenburg <ashleybahrenburg@gmail.com>
2016-10-05 12:46:28 -0400

    more javadoc

--- src/main/java/com/incredibly_humble/app/App.java
+++ src/main/java/com/incredibly_humble/app/App.java
@@ -12,12 +12,20 @@ import javafx.stage.Stage;
 
 import java.util.Arrays;
 
+/**
+ * a class to represent the application
+ */
 public class App extends Application {
 
     private GuiceContext context = new GuiceContext(this, () -> Arrays.asList(new Module()));
     @Inject
     private ScreenSwitch screenSwitch;
 
+    /**
+     * a method to start the app
+     * @param primaryStage the inital startup of the view
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         context.init();
@@ -26,7 +34,10 @@ public class App extends Application {
         primaryStage.show();
     }
 
-
+    /**
+     * launchs the program when run
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/com/incredibly_humble/app/util/Login.java
+++ src/main/java/com/incredibly_humble/app/util/Login.java
@@ -2,13 +2,16 @@ package com.incredibly_humble.app.util;
 
 import com.incredibly_humble.app.util.impl.exceptions.TriesExceededException;
 
+/**
+ * an interface to facilitate login
+ */
 public interface Login {
     /**
      * Method that takes in a username and password and will throw an error if there were three failed attempts
      * on this specific username
      * @param username - username for logging in
      * @param password - associated password
-     * @return
+     * @return a boolean whether login was successful or not
      * @throws TriesExceededException - thrown after three failed attempts.
      */
     public boolean verify(String username, String password) throws TriesExceededException;
