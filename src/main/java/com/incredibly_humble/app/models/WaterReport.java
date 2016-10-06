f156d2f0dd40b734a70439c743af08d1bfea22cb
Noam Lerner <noaml1295@gmail.com>
2016-10-06 18:23:20 -0400

    watertype model

---
+++ src/main/java/com/incredibly_humble/app/models/WaterReport.java
@@ -0,0 +1,62 @@
+package com.incredibly_humble.app.models;
+
+import java.util.Date;
+
+public class WaterReport {
+    public enum WaterType {
+        Bottled,
+        Well,
+        Stream,
+        Lake,
+        Spring,
+        Other
+    }
+
+    public enum WaterCondition {
+        Waste,
+        Treatable_Clear,
+        Treatable_Muddy,
+        Potable
+    }
+
+    private Date reported;
+    private int id;
+    private String workerName;
+    private String location;
+    private WaterType type;
+    private WaterCondition condition;
+
+    public WaterReport(Date reported, int id, String workerName, String location, WaterType type, WaterCondition condition){
+        this.reported = reported;
+        this.id = id;
+        this.workerName = workerName;
+        this.location = location;
+        this.type = type;
+        this.condition = condition;
+    }
+    
+    public Date getReported() {
+        return reported;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWorkerName() {
+        return workerName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public WaterType getType() {
+        return type;
+    }
+
+    public WaterCondition getCondition() {
+        return condition;
+    }
+
+}
