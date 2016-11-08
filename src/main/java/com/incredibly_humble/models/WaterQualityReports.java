5c6c9c3c9374745f42302e58f8bcfb8e27a4b76b
abahrenburg <ashleybahrenburg@gmail.com>
2016-11-08 18:53:37 -0500

    'javadocing'

--- src/main/java/com/incredibly_humble/models/WaterQualityReports.java
+++ src/main/java/com/incredibly_humble/models/WaterQualityReports.java
@@ -3,11 +3,24 @@ package com.incredibly_humble.models;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * a class representing the database of quality reports
+ */
 public class WaterQualityReports implements Serializable {
     private ArrayList<WaterQualityReport> reports;
+
+    /**
+     * constructs database
+     * @param reports the list of reports to be added
+     */
     public WaterQualityReports(ArrayList<WaterQualityReport> reports){
         this.reports = reports;
     }
+
+    /**
+     * gets the reports from the database
+     * @return the list of  quality reports
+     */
     public ArrayList<WaterQualityReport> getReports(){
         return reports;
     }
--- src/main/java/com/incredibly_humble/models/WaterSourceReports.java
+++ src/main/java/com/incredibly_humble/models/WaterSourceReports.java
@@ -3,11 +3,24 @@ package com.incredibly_humble.models;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * a class representing the source report database
+ */
 public class WaterSourceReports implements Serializable {
     private ArrayList<WaterSourceReport> reports;
+
+    /**
+     * a constructor for the database
+     * @param reports the list of reports to be inputted
+     */
     public WaterSourceReports(ArrayList<WaterSourceReport> reports){
         this.reports = reports;
     }
+
+    /**
+     * gets the list of reports
+     * @return a list of reports
+     */
     public ArrayList<WaterSourceReport> getReports(){
         return reports;
     }
